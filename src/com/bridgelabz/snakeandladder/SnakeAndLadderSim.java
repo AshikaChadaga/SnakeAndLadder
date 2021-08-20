158dbdd99889239ae6ee8a1818848a79f4c17c25
AshikaChadaga <ashikachadaga@gmail.com>
2021-08-20 16:11:25 +0530

    ADDED: Welcome to game message

---
+++ src/com/bridgelabz/snakeandladder/SnakeAndLadderSim.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.snakeandladder;
+
+public class SnakeAndLadderSim {
+
+	public static void main(String[] args) {
+		
+		System.out.println("---------- Welcome to the Game of SNAKES AND LADDERS!! ----------");
+
+	}
+
+}
