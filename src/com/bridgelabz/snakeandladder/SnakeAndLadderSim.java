b02162529a84baf810aebd99e52174294118e645
AshikaChadaga <ashikachadaga@gmail.com>
2021-08-20 16:20:29 +0530

    ADDED: UC2 Dice Num Generator

--- src/com/bridgelabz/snakeandladder/SnakeAndLadderSim.java
+++ src/com/bridgelabz/snakeandladder/SnakeAndLadderSim.java
@@ -1,12 +1,18 @@
 package com.bridgelabz.snakeandladder;
 
 public class SnakeAndLadderSim {
-
+	
+	public static int getRandomInteger(int maximum, int minimum){ 
+		return ((int) (Math.random()*(maximum - minimum))) + minimum; 
+	}
+	
 	public static void main(String[] args) {
 		
 		System.out.println("---------- Welcome to the Game of SNAKES AND LADDERS!! ----------");
 		
 		int startPositionA = 0;
+		int dice = getRandomInteger(7,1);
+		//System.out.println(dice);
 
 	}
 
